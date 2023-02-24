0a77fd427136f91a55085e9d0af953867253c398
Antônio Luis <antonioluisp97@gmail.com>
2023-02-24 14:35:01 -0300

    feat controller de envio de email para reset de senha e use case incompleto

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,38 @@
+import { injectable, inject } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
+
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: UsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: UsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("User does not Exists!");
+    }
+
+    const token = uuidV4();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      expires_date,
+      user_id: user.id,
+    });
+  }
+}
+export { SendForgotPasswordMailUseCase };
