ba8b165b0d3cd0f17e730a476d59c29f69312d57
Antônio Luis <antonioluisp97@gmail.com>
2023-02-24 15:32:41 -0300

    feat: provider de email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherialMailProvider } from "./MailProvider/implementations/EtherialMailProvider";
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherialMailProvider",
+  new EtherialMailProvider()
 );
