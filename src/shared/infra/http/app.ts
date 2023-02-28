4cf6e24007c324acd72c0c344641b0f9c1d0e4cd
Antônio Luis <antonioluisp97@gmail.com>
2023-02-27 23:15:29 -0300

    feat: profile user com limitacao de informacoes na api

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
+import upload from "@config/upload";
 import "express-async-errors";
 import { AppError } from "@shared/errors/AppError";
 
@@ -17,6 +18,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
